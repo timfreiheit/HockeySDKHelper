40e63624ab10bd0ba66c6a4e351d84bdaa463424
Tim Freiheit <timfreiheit@hotmail.de>
2018-02-26 17:40:27 +0100

    update to android gradle plugin 3.0.1

--- testapp/src/main/java/freiheit/testapp/MainActivity.java
+++ testapp/src/main/java/freiheit/testapp/MainActivity.java
@@ -1,14 +1,14 @@
 package freiheit.testapp;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import de.timfreiheit.hockey.utils.WarningExceptionHandler;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
